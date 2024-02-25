68a8d18cf285bf433d7889b6859e795e20d8985c
monisdx <135008013+monisdx@users.noreply.github.com>
2024-02-25 21:56:42 +0530

    deploy

--- components/Tabledata.jsx
+++ components/Tabledata.jsx
@@ -46,7 +46,7 @@ const Tabledata = ({
   };
 
   async function sendmail() {
-    const obj = { email: "moniskhandx@gmail.com", ids: selecteddata };
+    const obj = { email: "info@redpositive.in", ids: selecteddata };
 
     // console.log(obj);
     setselecteddata([]);
