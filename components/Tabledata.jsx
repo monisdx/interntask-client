e45ca6a2c9418e64853e394edaebed3078f632a3
monisdx <135008013+monisdx@users.noreply.github.com>
2024-02-25 19:39:52 +0530

    validation and email done

--- components/Tabledata.jsx
+++ components/Tabledata.jsx
@@ -1,58 +1,88 @@
+"use client";
 
-"use client"
-
-import  { useState } from 'react';
-import axios from 'axios';
-import { Table,  TableBody, TableCell, TableContainer, TableHead, TableRow, } from '@mui/material';
+import { useState } from "react";
+import axios from "axios";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { ImCheckboxUnchecked } from "react-icons/im";
 import { ImCheckboxChecked } from "react-icons/im";
 import { FaEdit } from "react-icons/fa";
 
-const Tabledata = ({setcurrentdata, fetchagain, setfetchagain, data, model, setmodel}) => {
+const Tabledata = ({
+  setcurrentdata,
+  fetchagain,
+  setfetchagain,
+  data,
+  model,
+  setmodel,
+}) => {
+  const [selecteddata, setselecteddata] = useState([]);
+  // const [toggle, settoggle] = useState(false);
 
-    const [selecteddata, setselecteddata] = useState([]);
-    const [toggle, settoggle] = useState(false);
+  console.log(selecteddata);
 
-    console.log(selecteddata)
-    const handleselection = (user) =>{
+  const handleselection = (id) => {
+    const bool = selecteddata.find((i) => i === id);
 
-        const bool = selecteddata.find((d)=> d?._id === user?._id);
-
-        if(bool){
-            setselecteddata(selecteddata.filter((d)=>d?._id !== user?._id))
-            settoggle(!toggle)
-        }
-        else{
-            setselecteddata([...selecteddata, user]);
-            settoggle(!toggle);
-        }
+    if (bool) {
+      setselecteddata(selecteddata.filter((i) => i !== id));
+    } else {
+      setselecteddata([...selecteddata, id]);
     }
-    const handledit = (user) => {
-        setcurrentdata(user);
-        setmodel(!model)
+    // settoggle(!toggle);
+  };
+
+  const handledit = (user) => {
+    setcurrentdata(user);
+    setmodel(!model);
+  };
+
+  async function sendmail() {
+    const obj = { email: "info@redpositive.in", ids: selecteddata };
+
+    // console.log(obj);
+    setselecteddata([]);
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/data/send-mail",
+        obj
+      );
+      console.log(data);
+      
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    const deletedata =async(id)=>{
-        try{
-        const {data} = await axios.delete(`http://localhost:5000/data/${id}`);
-        console.log(data);
-        setfetchagain(!fetchagain);
-
-        }
-        catch(error){
-            console.log(error)
-        }
-
+  const deletedata = async (id) => {
+    try {
+      const { data } = await axios.delete(`http://localhost:5000/data/${id}`);
+      console.log(data);
+      setfetchagain(!fetchagain);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
   return (
-    <div className='padding w-full  flex flex-col justify-center'>
-        <h1 className='manrope text-black font-extrabold p-4 text-2xl'>
-            Table 
-        </h1>
-        <button onClick={()=>setmodel(!model)} className="w-[150px] flex justify-center px-4 py-2 font-medium text-[18px] rounded-[10px] text-white bg-black outline-none border-none mb-4"> Add Entry</button>
-        {data?.length ? <div className="bg-white-100 flex flex-col gap-4  rounded-lg  p-5 w-full shadow-lg">
+    <div className="padding w-full  flex flex-col justify-center">
+      <h1 className="manrope text-black font-extrabold p-4 text-2xl">Table</h1>
+      <button
+        onClick={() => setmodel(!model)}
+        className="w-[150px] flex justify-center px-4 py-2 font-medium text-[18px] rounded-[10px] text-white bg-black outline-none border-none mb-4"
+      >
+        {" "}
+        Add Entry
+      </button>
+      {data?.length ? (
+        <div className="bg-white-100 flex flex-col gap-4  rounded-lg  p-5 w-full shadow-lg">
           <TableContainer>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
@@ -81,49 +111,80 @@ const Tabledata = ({setcurrentdata, fetchagain, setfetchagain, data, model, setm
                 </TableRow>
               </TableHead>
               <TableBody>
-                {data?.map((user,i)  => (
-                  <TableRow key={user._id} >
+                {data?.map((user, i) => (
+                  <TableRow key={user?._id}>
                     <TableCell>
-                        <div onClick={()=>handleselection(user)} className='cursor-pointer'>
-                            {toggle ? <ImCheckboxChecked size={'15px'} color='#1DB700'/> : <ImCheckboxUnchecked size={'15px'} color='#666768'/>}
-                        </div>
+                      <div
+                        onClick={() => handleselection(user?._id)}
+                        className="cursor-pointer"
+                      >
+                        {selecteddata.includes(user?._id) ? (
+                          <ImCheckboxChecked size={"15px"} color="#1DB700" />
+                        ) : (
+                          <ImCheckboxUnchecked size={"15px"} color="#666768" />
+                        )}
+                      </div>
                     </TableCell>
                     <TableCell>
-                      <h1 className="font-manrope text-grey">{i+1}</h1>
+                      <h1 className="font-manrope text-grey">{i + 1}</h1>
                     </TableCell>
                     <TableCell>
-                     <h1 className='font-medium text-grey'>{user.name}</h1>
+                      <h1 className="font-medium text-grey">{user.name}</h1>
                     </TableCell>
                     <TableCell>
                       <h1 className="font-medium text-grey">{user.email}</h1>
                     </TableCell>
                     <TableCell>
-                      <h1 className="font-medium text-grey">{user.phonenumber}</h1>
+                      <h1 className="font-medium text-grey">
+                        {user.phonenumber}
+                      </h1>
                     </TableCell>
                     <TableCell>
                       <div className=" flex flex-wrap gap-1">
-                        {user.hobbies.map((hobby,i)=>(
-                            <p key={i} className='font-medium text-grey'>{hobby}</p>
+                        {user.hobbies.map((hobby, i) => (
+                          <p key={i} className="font-medium text-grey">
+                            {hobby}
+                          </p>
                         ))}
-
                       </div>
                     </TableCell>
                     <TableCell>
-                      <div className='flex gap-[80px]'>
-                      <FaEdit size={'20px'} color='#2E8BC0' className='cursor-pointer' onClick={()=>handledit(user)}/>
-                      <RiDeleteBin6Line size={'20px'} color='#ff0000' className='cursor-pointer' onClick={()=>deletedata(user._id)}/>
-                      </div>  
+                      <div className="flex gap-[80px]">
+                        <FaEdit
+                          size={"20px"}
+                          color="#2E8BC0"
+                          className="cursor-pointer"
+                          onClick={() => handledit(user)}
+                        />
+                        <RiDeleteBin6Line
+                          size={"20px"}
+                          color="#ff0000"
+                          className="cursor-pointer"
+                          onClick={() => deletedata(user._id)}
+                        />
+                      </div>
                     </TableCell>
-                   
                   </TableRow>
-                 ))} 
+                ))}
               </TableBody>
             </Table>
           </TableContainer>
-        </div> : <h1 className='text=[20px] font-medium'>No entry yet</h1>}
-
+        </div>
+      ) : (
+        <h1 className="text=[20px] font-medium">no entry yet</h1>
+      )}
+      {data?.length ? (
+        <button
+          onClick={() => sendmail()}
+          className="w-[150px] flex justify-center px-4 py-2 font-medium text-[18px] rounded-[10px] text-white bg-black outline-none border-none mt-4"
+        >
+          Send Mail
+        </button>
+      ) : (
+        <div></div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Tabledata
+export default Tabledata;
